fbac135aaf095a55005f7a3c7ee97c811a2359db
JulienD <dubreuil.julien@gmail.com>
2013-05-21 15:15:50 +0200

    add shell executablr to launch the parser via the crontab

---
+++ scrape.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# be sure to change both virtualenv directory and scrape/living_social
+# directory to where your venv and code is.
+cd ~/Src/Python/drupal-projects-crawler
+/usr/local/bin/scrapy crawl ModulesXml
+
