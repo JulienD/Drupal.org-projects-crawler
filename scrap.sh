59ad000c70e26ee7b4f0212d28413c23fdca89de
JulienD <dubreuil.julien@gmail.com>
2014-04-20 16:34:24 +0200

    Added shell script to download the last version of the xml file and start the parser.

--- scrap.sh
+++ scrap.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-# be sure to change both virtualenv directory and scrape/living_social
-# directory to where your venv and code is.
-touch /tmp/test/test-scrap.txt
-cd /home/vagrant/public_html/python/drupal-projects-crawler
-/usr/local/bin/scrapy crawl ModulesXml
+download=false
+
+# Gets the XML file and gets its creation date.
+file_name="/tmp/drupal-project-release-7.xml"
+creation_date=$(date -r $file_name +%s)
+
+echo $creation_date
+
+# Checks if the xml file exists.
+if [[ -z $creation_date ]]
+then
+    # Mark the file has need to be downloaded.
+    download=true
+else
+    # Check the validity of the xml file.
+    current_time=$(date +%s)
+
+    # Convert diff in minutes.
+    (( diff=diff/3600 ))
+
+    if ($diff > 24)
+    then
+        download=true
+    fi
+fi
+
+if ($download = true)
+then
+    curl -o /tmp/drupal-project-release-7.xml http://updates.drupal.org/release-history/project-list/all/7
+fi
+
+/usr/bin/scrapy crawl ModulesXml
